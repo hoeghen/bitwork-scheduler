66022240e1a2f5a134865b286331e54d978f407a
christian <carverdk@yahoo.dk>
2016-12-17 10:16:27 +0100

    remove env

--- app.js
+++ app.js
@@ -7,6 +7,5 @@ app.get('/', function (req, res) {
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('version 3 listening on port 3000!')
-  console.log("environment="+JSON.stringify(process.env))
 })
 
