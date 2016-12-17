f269079205ca8a099cd8abfd6e1b73e6beb9433c
christian <carverdk@yahoo.dk>
2016-12-17 09:54:07 +0100

    add port

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname+"/index.html")
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('version 3 listening on port 3000!')
 })
